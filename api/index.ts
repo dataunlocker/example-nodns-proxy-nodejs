28422df542673d7e8d569bbb1d895430717bf084
Nikita Savchenko <zitros.lab@gmail.com>
2021-05-30 14:08:08 +0300

    Update code to distinguish environments

--- api/index.ts
+++ api/index.ts
@@ -1,33 +1,50 @@
+// A simple reverse proxy forwarding traffic from https://your-website.com/proxy-path/*
+// to https://dataunlocker-key.ddns.your-website.com/*, adding additional `x-du-ip` header.
+// This script uses environment variables
+
 import express from "express";
 import proxy from "express-http-proxy";
-import serveStatic from "serve-static";
+import { readFileSync } from "fs";
+
+const publicDir = "api/public";
+const scriptsDir = "api/scripts";
 
 // Reading environment variables passed by Vercel. These are all provided by DataUnlocker.
-// X_DU_TOKEN - the token header for authorization purposes.
 // PATH_PREFIX - a path prefix at the current domain to use for everything which is proxied.
 // DATAUNLOCKER_HOST - a proxy domain address (DataUnlocker's endpoint).
-const X_DU_TOKEN = process.env.X_DU_TOKEN;
-const PATH_PREFIX = "/" + (process.env.PATH_PREFIX || "").replace(/.*\//, "");
+// DATAUNLOCKER_SCRIPT_NAME - a name of the DataUnlocker script which will be served (from "scripts" directory).
+const PATH_PREFIX =
+  "/" + (process.env.PATH_PREFIX || "no-path-prefix").replace(/.*\//, "");
 const DATAUNLOCKER_HOST = (
   process.env.DATAUNLOCKER_HOST || "undefined"
 ).replace(/^(?:https?:\/\/)?/, "https://");
+const DATAUNLOCKER_SCRIPT_NAME =
+  process.env.DATAUNLOCKER_SCRIPT_NAME || "production";
 
-// Everything below - the proxy application logic.
+// Served files (as we replace some things it needs to be dynamic).
+const servedJsFileName = `scripts/${DATAUNLOCKER_SCRIPT_NAME}.js`;
+console.log(`Reading served HTML file ${servedJsFileName}`);
+const servedJs = readFileSync(
+  `${scriptsDir}/${DATAUNLOCKER_SCRIPT_NAME}.js`
+).toString("utf8");
+const servedFavicon = readFileSync(`${publicDir}/favicon.ico`);
+const servedHtml = readFileSync(`${publicDir}/index.html`)
+  .toString("utf8")
+  .replace("/*DATAUNLOCKER_SCRIPT_NAME*/", () => servedJs);
+
 console.log(
-  `Proxy configured for ${DATAUNLOCKER_HOST} with path prefix ${PATH_PREFIX}. Token was ${
-    X_DU_TOKEN ? "" : "NOT "
-  }set.`
+  `Proxy has been configured for ${DATAUNLOCKER_HOST} with path prefix ${PATH_PREFIX}.`
 );
 
+// The proxy application logic.
 const app = express();
-app.set("trust proxy", true); // Because Vercel (vercel.com) works behind the proxy itself.
+app.set("trust proxy", true); // Required because Vercel (vercel.com) works behind the proxy itself.
 app.use(
   PATH_PREFIX,
   proxy(`${DATAUNLOCKER_HOST}`, {
     memoizeHost: true,
     proxyReqOptDecorator: (proxyRequest, req) => {
-      proxyRequest.headers["X-Du-Token"] = X_DU_TOKEN;
-      proxyRequest.headers["X-Du-IP"] = req.ip;
+      proxyRequest.headers["x-du-ip"] = req.ip;
       console.log(
         `${proxyRequest.path} -> ${DATAUNLOCKER_HOST}${proxyRequest.path}`
       );
@@ -35,6 +52,15 @@ app.use(
     },
   })
 );
-app.use(serveStatic("public", { index: ["index.html"] }));
+
+// Static serving.
+app.get("/favicon.ico", (_, res) => {
+  res.setHeader("Content-Type", "image/x-icon");
+  res.send(servedFavicon);
+});
+app.get("/", (_, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send(servedHtml);
+});
 
 export default app;
